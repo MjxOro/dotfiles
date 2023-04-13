8dbd3e4430e84b04eb8e639632a4f826b4dbd686
MjxOro <mattheweoro@gmail.com>
2023-04-12 22:23:34 -0400

    update neovim config

--- stow/nvim/.config/nvim/lua/mjx/remap.lua
+++ stow/nvim/.config/nvim/lua/mjx/remap.lua
@@ -1,5 +1,5 @@
 -- Turn <Leader> to space
-vim.g.mapleader = " "
+-- vim.g.mapleader = " "
 -- Highlight Line Mover
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
