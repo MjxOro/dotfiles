79944f4b2db6de6fb71777d2cb357bca5865c45e
MjxOro <mattheweoro@gmail.com>
2024-10-28 20:00:26 -0400

    test update on 10/28/24

--- installs/install_linux.sh
+++ installs/install_linux.sh
@@ -11,7 +11,13 @@ git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerl
 echo "Installing neovim"
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
-./nvim.appimage
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
+
+# Optional: exposing nvim globally.
+sudo mv squashfs-root /
+sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+nvim
 
 # NODE VERSION MANAGER INSTALLS
 # NVM directory already setup in .zshrc
