66cd83a9a6975fd268d42975647bb74ec6c25fab
MjxOro <mattheweoro@gmail.com>
2025-03-30 22:14:33 -0400

    WIP: Current Config

--- stow/nvim/.config/nvim/lua/plugins/cmp.lua
+++ stow/nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,44 +1,146 @@
+-- Place this in ~/.config/nvim/lua/plugins/cmp.lua
+-- return {
+--   {
+--     "hrsh7th/nvim-cmp",
+--     dependencies = {
+--       "hrsh7th/cmp-nvim-lsp",
+--       "hrsh7th/cmp-buffer",
+--       "hrsh7th/cmp-path",
+--       "saadparwaiz1/cmp_luasnip",
+--     },
+--     opts = function(_, opts)
+--       local cmp = require("cmp")
+--
+--       opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+--         -- Use Tab to move to next item in completion menu
+--         ["<Tab>"] = cmp.mapping(function(fallback)
+--           if cmp.visible() then
+--             cmp.select_next_item()
+--           else
+--             fallback()
+--           end
+--         end, { "i", "s" }),
+--
+--         -- Use Shift+Tab to move to previous item
+--         ["<S-Tab>"] = cmp.mapping(function(fallback)
+--           if cmp.visible() then
+--             cmp.select_prev_item()
+--           else
+--             fallback()
+--           end
+--         end, { "i", "s" }),
+--
+--         -- Use Enter to confirm selection
+--         ["<CR>"] = cmp.mapping.confirm({ select = true }),
+--
+--         -- Uncomment below to disable the default tab behavior completely
+--         -- ["<Tab>"] = cmp.mapping.disable,
+--       })
+--
+--       -- Optional: Disable automatic selection while navigating
+--       opts.preselect = cmp.PreselectMode.None
+--
+--       return opts
+--     end,
+--   },
+-- }
+-- Place this in ~/.config/nvim/lua/plugins/cmp.lua
+
+-- return {
+--   {
+--     "hrsh7th/nvim-cmp",
+--     -- Don't use opts = function(), instead use direct config
+--     config = function()
+--       local cmp = require("cmp")
+--       cmp.setup({
+--         mapping = {
+--           ["<Tab>"] = cmp.mapping(function(fallback)
+--             if cmp.visible() then
+--               cmp.select_next_item()
+--             else
+--               fallback()
+--             end
+--           end, { "i", "s" }),
+--
+--           ["<S-Tab>"] = cmp.mapping(function(fallback)
+--             if cmp.visible() then
+--               cmp.select_prev_item()
+--             else
+--               fallback()
+--             end
+--           end, { "i", "s" }),
+--
+--           ["<CR>"] = cmp.mapping.confirm({ select = true }),
+--
+--           -- Keep the default menu display mapping
+--           ["<C-Space>"] = cmp.mapping.complete(),
+--         },
+--         -- Other important settings you might want to keep
+--         sources = {
+--           { name = "nvim_lsp" },
+--           { name = "buffer" },
+--           { name = "path" },
+--           { name = "luasnip" },
+--         },
+--         -- Make menu appear on first key press
+--         completion = {
+--           completeopt = "menu,menuone,noinsert",
+--         },
+--       })
+--     end,
+--   },
+-- }
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
+    "saghen/blink.cmp",
+    -- optional: provides snippets for the snippet source
+    dependencies = "rafamadriz/friendly-snippets",
+
+    -- use a release tag to download pre-built binaries
+    version = "*",
+    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+    -- build = 'cargo build --release',
+    -- If you use nix, you can build from source using latest nightly rust with:
+    -- build = 'nix run .#build-plugin',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept, C-n/C-p for up/down)
+      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys for up/down)
+      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+      --
+      -- All presets have the following mappings:
+      -- C-space: Open menu or open docs if already open
+      -- C-e: Hide menu
+      -- C-k: Toggle signature help
+      --
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = { preset = "default" },
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+        -- Useful for when your theme doesn't support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+        -- Adjusts spacing to ensure icons are aligned
+        nerd_font_variant = "mono",
+      },
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+
+      -- Blink.cmp uses a Rust fuzzy matcher by default for typo resistance and significantly better performance
+      -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+      -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+      --
+      -- See the fuzzy documentation for more information
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
+    opts_extend = { "sources.default" },
   },
 }
